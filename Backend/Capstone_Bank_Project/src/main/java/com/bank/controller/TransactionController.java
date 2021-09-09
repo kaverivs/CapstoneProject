120267e1e43b30f6119dfeabdbd0b6c518123549
kaverivs <vskaveri2016@gmail.com>
2021-09-09 11:32:35 +0530

    Add files via upload

---
+++ Backend/Capstone_Bank_Project/src/main/java/com/bank/controller/TransactionController.java
@@ -0,0 +1,34 @@
+package com.bank.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bank.dto.BankResponseDTO;
+import com.bank.dto.TransactionResponseDTO;
+import com.bank.model.Customer_Details;
+import com.bank.model.Transaction;
+import com.bank.service.TransactionService;
+
+
+@RestController
+@RequestMapping(value="/transaction")
+public class TransactionController {
+	
+	@Autowired
+	private TransactionService transactionService;
+	
+	/*--------------------------------Transaction ---------------------------*/
+	@RequestMapping(value = "/transfer",consumes = "application/json",produces = "application/json",method = RequestMethod.POST)
+	public ResponseEntity<TransactionResponseDTO>transfer(@RequestBody Transaction transaction ){
+		TransactionResponseDTO response=transactionService.transfer(transaction);
+		return new ResponseEntity<>(response,HttpStatus.OK);
+	}
+	
+
+}
